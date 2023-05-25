d1be0abeb4b95060069b1f8889f6561ba4f593a6
MArtin Amankwaa <martyofmca@gmail.com>
2023-05-25 02:37:45 +0100

    split a string to a pointer to strings

---
+++ _token_count.c
@@ -0,0 +1,28 @@
+#include "mmain.h"
+
+/**
+ * _tokencount - Counts the number of tokens in the given command
+ *
+ * @str: The command to be executed including command line arguments if any
+ * @delimiter: The delimiting character(s)
+ *
+ * Return: The number of tokens found in the given command
+*/
+int _tokencount(char *str, char *delimiter)
+{
+	int token_count = 0, length = 0;
+	char *token = NULL, *tmp = NULL;
+
+	length = _strlen(str);
+	tmp = malloc(sizeof(char) * length);
+	_strcpy(tmp, str);
+	token = strtok(tmp, delimiter);
+	while (token != NULL)
+	{
+		token_count++;
+		token = strtok(NULL, delimiter);
+	}
+	_freestr(tmp);
+	return (token_count);
+}
+
